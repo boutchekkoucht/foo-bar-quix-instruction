c3b04ab619c8d143b2b6b8fd2efb6720c7257dd4
Mohammed BOUTCHEKKOUCHT <boutchekkoucht@gmail.com>
2020-12-07 22:32:26 +0100

    wirte the service for te get conversion form back end

--- foobarquix-ui/src/app/foo-bar-quix.service.ts
+++ foobarquix-ui/src/app/foo-bar-quix.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FooBarQuixService {
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public convertInput(inputNumber: number) {
+    return this.http.get(environment.urlBackend + inputNumber);
+  }
 
 }
